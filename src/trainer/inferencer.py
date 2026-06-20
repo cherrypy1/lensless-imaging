c3b5ce40671145806049fbc676f611059bbe85d9
Artem <chameleonrichi7@gmail.com>
2026-06-20 16:13:08 +0300

    added trainer

---
+++ src/trainer/inferencer.py
@@ -0,0 +1,205 @@
+from time import perf_counter
+
+import torch
+from tqdm.auto import tqdm
+
+from src.metrics.tracker import MetricTracker
+from src.datasets.image_utils import save_tensor_image
+from src.trainer.base_trainer import BaseTrainer
+
+
+class Inferencer(BaseTrainer):
+    """
+    Inferencer (Like Trainer but for Inference) class
+
+    The class is used to process data without
+    the need of optimizers, writers, etc.
+    Required to evaluate the model on the dataset, save predictions, etc.
+    """
+
+    def __init__(
+        self,
+        model,
+        config,
+        device,
+        dataloaders,
+        save_path,
+        metrics=None,
+        batch_transforms=None,
+        skip_model_load=False,
+    ):
+        """
+        Initialize the Inferencer.
+
+        Args:
+            model (nn.Module): PyTorch model.
+            config (DictConfig): run config containing inferencer config.
+            device (str): device for tensors and model.
+            dataloaders (dict[DataLoader]): dataloaders for different
+                sets of data.
+            save_path (str): path to save model predictions and other
+                information.
+            metrics (dict): dict with the definition of metrics for
+                inference (metrics[inference]). Each metric is an instance
+                of src.metrics.BaseMetric.
+            batch_transforms (dict[nn.Module] | None): transforms that
+                should be applied on the whole batch. Depend on the
+                tensor name.
+            skip_model_load (bool): if False, require the user to set
+                pre-trained checkpoint path. Set this argument to True if
+                the model desirable weights are defined outside of the
+                Inferencer Class.
+        """
+        assert (
+            skip_model_load or config.inferencer.get("from_pretrained") is not None
+        ), "Provide checkpoint or set skip_model_load=True"
+
+        self.config = config
+        self.cfg_trainer = self.config.inferencer
+
+        self.device = device
+
+        self.model = model
+        self.batch_transforms = batch_transforms
+        self.times = []
+        self.image_count = 0
+
+        # define dataloaders
+        self.evaluation_dataloaders = {k: v for k, v in dataloaders.items()}
+
+        # path definition
+
+        self.save_path = save_path
+
+        # define metrics
+        self.metrics = metrics
+        if self.metrics is not None:
+            self.evaluation_metrics = MetricTracker(
+                *[m.name for m in self.metrics["inference"]],
+                writer=None,
+            )
+        else:
+            self.evaluation_metrics = None
+
+        if not skip_model_load:
+            # init model
+            self._from_pretrained(config.inferencer.get("from_pretrained"))
+
+    def run_inference(self):
+        """
+        Run inference on each partition.
+
+        Returns:
+            part_logs (dict): part_logs[part_name] contains logs
+                for the part_name partition.
+        """
+        part_logs = {}
+        for part, dataloader in self.evaluation_dataloaders.items():
+            logs = self._inference_part(part, dataloader)
+            part_logs[part] = logs
+        if self.config.inferencer.get("count_time") and self.image_count > 0:
+            value = sum(self.times) / self.image_count
+            print(f"среднее время реконструкции изображения (сек): {value:.4f}")
+        return part_logs
+
+    def process_batch(self, batch_idx, batch, metrics, part):
+        """
+        Run batch through the model, compute metrics, and
+        save predictions to disk.
+
+        Save directory is defined by save_path in the inference config.
+
+        Args:
+            batch_idx (int): the index of the current batch.
+            batch (dict): dict-based batch containing the data from
+                the dataloader.
+            metrics (MetricTracker): MetricTracker object that computes
+                and aggregates the metrics. The metrics depend on the type
+                of the partition (train or inference).
+            part (str): name of the partition. Used to define proper saving
+                directory.
+        Returns:
+            batch (dict): dict-based batch containing the data from
+                the dataloader (possibly transformed via batch transform)
+                and model outputs.
+        """
+        batch = self.move_batch_to_device(batch)
+        batch = self.transform_batch(batch)  # transform batch on device -- faster
+
+        if self.config.inferencer.get("count_time"):
+            if self.device == "cuda":
+                torch.cuda.synchronize()
+            start = perf_counter()
+            outputs = self.model(**batch)
+            if self.device == "cuda":
+                torch.cuda.synchronize()
+            self.times.append(perf_counter() - start)
+        else:
+            outputs = self.model(**batch)
+        batch.update(outputs)
+        batch_size = batch["reconstruction_roi"].shape[0]
+        self.image_count += batch_size
+
+        if metrics is not None and "target_roi" in batch:
+            for met in self.metrics["inference"]:
+                metrics.update(met.name, met(**batch), n=batch_size)
+            self.metric_count += batch_size
+
+        # Some saving logic. This is an example
+        # Use if you need to save predictions on disk
+
+        for i in range(batch_size):
+            # clone because of
+            # https://github.com/pytorch/pytorch/issues/1995
+            image = batch["reconstruction_roi"][i].clone()
+            if self.save_path is not None:
+                # you can use safetensors or other lib here
+                image_id = batch["id"][i]
+                save_tensor_image(
+                    image,
+                    self.save_path / f"{image_id}.png",
+                )
+
+        return batch
+
+    def _inference_part(self, part, dataloader):
+        """
+        Run inference on a given partition and save predictions
+
+        Args:
+            part (str): name of the partition.
+            dataloader (DataLoader): dataloader for the given partition.
+        Returns:
+            logs (dict): metrics, calculated on the partition.
+        """
+
+        self.is_train = False
+        self.model.eval()
+        self.metric_count = 0
+
+        if self.evaluation_metrics is not None:
+            self.evaluation_metrics.reset()
+
+        # create Save dir
+        if self.save_path is not None:
+            self.save_path.mkdir(exist_ok=True, parents=True)
+
+        with torch.no_grad():
+            for batch_idx, batch in tqdm(
+                enumerate(dataloader),
+                desc=part,
+                total=len(dataloader),
+            ):
+                count = self.config.inferencer.get("count")
+                if count is not None and self.image_count >= count:
+                    break
+                batch = self.process_batch(
+                    batch_idx=batch_idx,
+                    batch=batch,
+                    part=part,
+                    metrics=self.evaluation_metrics,
+                )
+
+        if self.evaluation_metrics is None or self.metric_count == 0:
+            return {}
+        return self.evaluation_metrics.result()
